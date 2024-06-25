9143c79ae43ac31a31d6b997bfc6141a3ce87e57
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2024-06-25 08:33:38 -0300

    Subindo projeto inicial

---
+++ src/main/java/br/com/adotePet/api_adotePet/ApiAdotePetApplication.java
@@ -0,0 +1,13 @@
+package br.com.adotePet.api_adotePet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdotePetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAdotePetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/adotePet/api_adotePet/ApiAdotePetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adotePet.api_adotePet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAdotePetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
