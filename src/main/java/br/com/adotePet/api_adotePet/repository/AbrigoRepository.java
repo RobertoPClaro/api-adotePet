f29ad44011f93df391450ec757fca8f699d55409
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2024-06-25 10:39:25 -0300

    Criando AbrigoRepository

---
+++ src/main/java/br/com/adotePet/api_adotePet/repository/AbrigoRepository.java
@@ -0,0 +1,7 @@
+package br.com.adotePet.api_adotePet.repository;
+
+import br.com.adotePet.api_adotePet.entity.Abrigo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AbrigoRepository extends JpaRepository<Abrigo, Long> {
+}
