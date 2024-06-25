1f3482b3bf9141527857129900e3b4700feb471a
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2024-06-25 10:42:22 -0300

    Criando TutorRepository

---
+++ src/main/java/br/com/adotePet/api_adotePet/repository/TutorRepository.java
@@ -0,0 +1,7 @@
+package br.com.adotePet.api_adotePet.repository;
+
+import br.com.adotePet.api_adotePet.entity.Tutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TutorRepository extends JpaRepository<Tutor, Long> {
+}
