44533a6ab156a18d7da3ead290ac321a8901a2b5
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2024-06-25 09:52:13 -0300

    Refatorando entidade Tutor

--- src/main/java/br/com/adotePet/api_adotePet/entity/Tutor.java
+++ src/main/java/br/com/adotePet/api_adotePet/entity/Tutor.java
@@ -1,7 +1,10 @@
 package br.com.adotePet.api_adotePet.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 import java.util.List;
 import java.util.Objects;
@@ -9,10 +12,26 @@ import java.util.Objects;
 @Entity
 @Table(name="tutores")
 public class Tutor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @NotBlank
+    @Column(name = "nome")
     private String nome;
+
+    @NotBlank
+    @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}")
+    @Column(name = "telefone")
     private String telefone;
+
+    @NotBlank
+    @Email
+    @Column(name = "email")
     private String email;
+    @OneToMany(mappedBy = "tutor", fetch = FetchType.EAGER)
+    @JsonManagedReference("tutor_adocoes")
     private List<Adocao> adocoes;
 
     @Override
